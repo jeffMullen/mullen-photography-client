eedf7c0ddfb5ba8c7bf4b7adff53b0cae36b6ea0
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-11-23 15:21:10 -0500

    importing mui app/navbar into header element

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import CssBaseLine from '@mui/material/CssBaseline';
+import './scss/styles.scss';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseLine />
+      <div className="App">
+        <header className="App-header">
+          <Navbar />
+        </header>
+      </div>
+    </React.Fragment>
   );
 }
 
