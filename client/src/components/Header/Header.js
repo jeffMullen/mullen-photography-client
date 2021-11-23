b3f2cf75df98357235c571b5365bb5095d9bc819
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-11-23 16:45:13 -0500

    cleaning up header code; consolidating all header code in singular header component file

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,16 +1,87 @@
-import React from 'react';
-import Navbar from './Navbar/Navbar';
-// import styles from './Header.module.scss';
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
 
+// AppBar imports
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuOpenIcon from '@mui/icons-material/MenuOpen';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
-function Header() {
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+// import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 
+// Styles
+import styles from './Header.module.scss';
+
+function NavDrawer() {
+    const [drawerState, setDrawerState] = useState({ left: false });
+
+    const anchor = 'left';
+
+    const toggleDrawer = (anchor, open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        setDrawerState({ [anchor]: open });
+    };
+
+    const list = (anchor) => (
+        <Box
+            role="presentation"
+            onClick={toggleDrawer(anchor, false)}
+            onKeyDown={toggleDrawer(anchor, false)}
+        >
+            <List>
+                {['About', 'Gallery'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+    );
 
     return (
         <>
-            <Navbar />
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Drawer
+                            anchor={anchor}
+                            open={drawerState[anchor]}
+                            onClose={toggleDrawer(anchor, false)}>
+
+                            <div
+                                className={styles.drawer}>
+                                {list(anchor)}
+                            </div>
+                        </Drawer>
+                        <IconButton
+                            onClick={toggleDrawer(anchor, true)}
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{ mr: 2 }}
+                        >
+                            <MenuOpenIcon />
+                        </IconButton>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                            Mullen Photography
+                        </Typography>
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+
         </>
     );
 }
 
-export default Header;
+export default NavDrawer;
