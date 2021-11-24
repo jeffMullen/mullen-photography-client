c3e12f47fb3eb4ff95fa5a8553e88588926961a8
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-11-24 13:23:29 -0500

    starting Theme file to surround app

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,22 @@ import CssBaseLine from '@mui/material/CssBaseline';
 import './scss/styles.scss';
 import Header from './components/Header/Header';
 
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './utils/Theme';
+
 function App() {
+
+  console.log(theme)
   return (
     <>
       <CssBaseLine />
-      <div className="App">
-        <header className="App-header">
-          <Header />
-        </header>
-      </div>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <header className="App-header">
+            <Header />
+          </header>
+        </div>
+      </ThemeProvider>
     </>
   );
 }
---
+++ client/src/utils/Theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#F7F7F7',
+            light: '#F7F7F7',
+            dark: '#EFEFEF',
+        },
+        secondary: {
+            main: '#143109',
+            light: '#143109',
+            dark: '#143109',
+        }
+    }
+})
+
+export default theme;
