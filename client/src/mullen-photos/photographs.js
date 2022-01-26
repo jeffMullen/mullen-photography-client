166fcec11f398ccf3b5a3e28ff6530a2e389007f
Walrus <81767572+jeffMullen@users.noreply.github.com>
2022-01-26 15:17:30 -0500

    added 25 photos to display

--- client/src/mullen-photos/photographs.js
+++ client/src/mullen-photos/photographs.js
@@ -1,4 +1,28 @@
 import portland from './portland.jpeg';
+import berryPortrait from './berries-portrait-downsize.jpeg';
+import sedona from './sedona.jpeg';
+import sunset from './sunset.jpeg';
+import cactus from './cactus.jpeg';
+import iris from './iris.jpeg';
+import camera from './camera.jpeg';
+import hawthorneBridge from './hawthorne-bridge.jpeg';
+import groundLife from './ground-life.jpeg';
+import mtSaintHelens from './mt-saint-helens.jpeg';
+import problematic from './problematic.jpeg';
+import anenome from './anenome.jpeg';
+import ducks from './ducks.jpeg';
+import dirtMushroom from './dirt-mushroom.jpeg';
+import lenseLight from './lense-light.jpeg';
+import wateringHole from './watering-hole.jpeg';
+import bleachedRunning from './bleached-running.jpeg';
+import whiteout from './whiteout.jpeg';
+import gaze from './gaze.jpeg';
+import threeWatchers from './three-watchers.jpeg';
+import oasis from './oasis.jpeg';
+import greenAndPurple from './green-purple.jpeg';
+import magnolia from './magnolia.jpeg';
+import trillium from './trillium.jpeg';
+import fairyHome from './fairy-home.jpeg';
 
 const imageData = [
     {
@@ -7,6 +31,152 @@ const imageData = [
         title: 'Portland',
         description: "Al's Den in downtown Portland",
         category: 'Urban'
+    },
+    {
+        img: berryPortrait,
+        photographer: 'Jeff Mullen',
+        title: 'Berries',
+        description: "Ankeny street berries",
+        category: 'Landscape'
+    },
+    {
+        img: sunset,
+        photographer: 'Jeff Mullen',
+        title: 'Sunset',
+        description: 'Cannon Beach - Oregon',
+        category: 'Landscape'
+    },
+    {
+        img: cactus,
+        photographer: 'Jeff Mullen',
+        title: 'Cactus',
+        category: 'Landscape'
+    },
+    {
+        img: iris,
+        photographer: 'Jeff Mullen',
+        title: 'Iris',
+        category: 'Landscape'
+    },
+    {
+        img: camera,
+        photographer: 'Jeff Mullen',
+        title: 'Camera',
+        category: 'Urban'
+    },
+    {
+        img: hawthorneBridge,
+        photographer: 'Jeff Mullen',
+        title: 'Hawthorne Bridge',
+        category: 'Urban'
+    },
+    {
+        img: groundLife,
+        photographer: 'Jeff Mullen',
+        title: 'Life From Below',
+        category: 'Landscape'
+    },
+    {
+        img: mtSaintHelens,
+        photographer: 'Jeff Mullen',
+        title: 'Mt. Saint Helens',
+        category: 'Landscape'
+    },
+    {
+        img: problematic,
+        photographer: 'Jeff Mullen',
+        title: 'Problematic',
+        category: 'Urban'
+    },
+    {
+        img: anenome,
+        photographer: 'Jeff Mullen',
+        title: 'Sea Anenome',
+        category: 'Landscape'
+    },
+    {
+        img: ducks,
+        photographer: 'Jeff Mullen',
+        title: 'Ducks',
+        category: 'Urban'
+    },
+    {
+        img: dirtMushroom,
+        photographer: 'Jeff Mullen',
+        title: 'Mushroom',
+        category: 'Landscape'
+    },
+    {
+        img: lenseLight,
+        photographer: 'Jeff Mullen',
+        title: 'Lense Light',
+        category: 'Abstract'
+    },
+    {
+        img: wateringHole,
+        photographer: 'Jeff Mullen',
+        title: 'The Great Watering Hole',
+        category: 'Urban'
+    },
+    {
+        img: bleachedRunning,
+        photographer: 'Jeff Mullen',
+        title: 'Bleached Running',
+        category: 'Abstract'
+    },
+    {
+        img: whiteout,
+        photographer: 'Jeff Mullen',
+        title: 'Whiteout',
+        category: 'Abstract'
+    },
+    {
+        img: gaze,
+        photographer: 'Jeff Mullen',
+        title: 'Gaze',
+        category: 'Abstract'
+    },
+    {
+        img: threeWatchers,
+        photographer: 'Jeff Mullen',
+        title: 'Three Watchers',
+        category: 'Landscape'
+    },
+    {
+        img: oasis,
+        photographer: 'Jeff Mullen',
+        title: 'Oasis',
+        category: 'Landscape'
+    },
+    {
+        img: greenAndPurple,
+        photographer: 'Jeff Mullen',
+        title: 'Green and Purple',
+        category: 'Landscape'
+    },
+    {
+        img: magnolia,
+        photographer: 'Jeff Mullen',
+        title: 'Magnolia',
+        category: 'Landscape'
+    },
+    {
+        img: trillium,
+        photographer: 'Jeff Mullen',
+        title: 'Trillium',
+        category: 'Landscape'
+    },
+    {
+        img: fairyHome,
+        photographer: 'Jeff Mullen',
+        title: 'Fairy Home',
+        category: 'Landscape'
+    },
+    {
+        img: sedona,
+        photographer: 'Jeff Mullen',
+        title: 'Sedona',
+        category: 'Landscape'
     },
     {
         img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
